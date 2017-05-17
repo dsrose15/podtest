2d4c2140d288146aa495550a771f25c43783f3e8
Scott Rose <srose@woolbright.net>
2017-05-17 09:36:13 -0400

    Initial Commit

---
+++ podtest/podtest.h
@@ -0,0 +1,19 @@
+//
+//  podtest.h
+//  podtest
+//
+//  Created by scott rose on 5/17/17.
+//  Copyright © 2017 Woolbright Development Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for podtest.
+FOUNDATION_EXPORT double podtestVersionNumber;
+
+//! Project version string for podtest.
+FOUNDATION_EXPORT const unsigned char podtestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podtest/PublicHeader.h>
+
+
