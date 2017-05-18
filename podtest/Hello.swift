32a1a7733207c9fefb266c60cd88be864c08025a
Scott Rose <srose@woolbright.net>
2017-05-18 10:57:36 -0400

    Initial update

---
+++ podtest/Hello.swift
@@ -0,0 +1,16 @@
+//
+//  Hello.swift
+//  podtest
+//
+//  Created by scott rose on 5/18/17.
+//  Copyright © 2017 Woolbright Development Inc. All rights reserved.
+//
+
+import Foundation
+
+class Hello {
+    
+    class func Say() {
+        print("Hello")
+    }
+}
